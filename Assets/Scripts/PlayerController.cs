ff578ae954f0c0e93c810b882f14f7c1c7ccc78e
Rookieamatuer <15160519320@163.com>
2023-10-08 21:52:14 +0800

    Merge

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+    private Vector2 moveValue;
+    private int count;
+    private int numPickups = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        winText.text = "";      
+        SetCountText();
+    }
+
+    // Update is called once per frame
+/*    void Update()
+    {
+
+    }*/
+
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp") 
+        { 
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+}
